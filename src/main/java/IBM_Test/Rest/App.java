6b7744b1564e550fc0c25730cf5ec0c93b88f781
Daniel Haugen <daniel.haugen1989@gmail.com>
2017-09-13 10:18:50 +0200

    Vår första commit

---
+++ src/main/java/IBM_Test/Rest/App.java
@@ -0,0 +1,13 @@
+package IBM_Test.Rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
